a4e95bb72787765a095161f5794612fe97a629e1
gbengasaolu <92299950+gbengasaolu@users.noreply.github.com>
2025-10-11 08:25:35 -0400

    Enhanced login & signup pages with brand styling and Firebase functionality

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// /js/firebase-config.js
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAgBhkRDr24rL5lCanJNqj9jfvMIrbODrY",
   authDomain: "si-analytics-academy.firebaseapp.com",
   projectId: "si-analytics-academy",
@@ -15,7 +10,3 @@ const firebaseConfig = {
   appId: "1:162447510839:web:75b4348257be24552e5eb2",
   measurementId: "G-6H1L04S7QM"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
